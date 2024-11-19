32a7a7cafb22a432c945318fa33e373838f1cba0
B1nus <123@1>
2024-11-19 21:43:34 +0100

    I tried, maybe I'm of by one for whatever reason

--- 21.zig
+++ 21.zig
@@ -94,13 +94,11 @@ const input = @embedFile("21.txt");
 // Tiny edges, There are 202300 per direction and they can be simulated by stepping 64 times
 // from the corresponding corner.
 //
-// Large edges, There are 202299 per direction and they can be simulated by stepping 131 + 65 times
+// Large edges, There are 202299 per direction and they can be simulated by stepping 65 * 2 + 65 times
 // from the corresponding corner.
 //
 // You have to simulate one of these for each direction since they have different obstacles to depending
 // on the direction.
-//
-// So,
 pub fn main() !void {
     var lines = std.mem.splitScalar(u8, input, '\n');
     const width = lines.peek().?.len;
@@ -112,12 +110,73 @@ pub fn main() !void {
         i += width;
     }
 
-    var sum: usize = 0;
-    for (0..width * height) |j| {
-        if (j % 2 == 0 and grid[j] != '#') {
-            sum += 1;
-        }
-    }
+    const uneven = try step(grid, width, width / 2 + height / 2 * width, 131, false);
+    const even = try step(grid, width, width / 2 + height / 2 * width, 132, false);
 
-    std.debug.print("Day 21 >> {d}\n", .{width});
+    // Uneven grids = 10231221350 * 4
+    // Even grids = 10231322500 * 4
+    const grid_sum: u128 = (1023122135 * uneven + 10231322500 * even) * 4 + uneven; // Adding one because of the starting grid
+
+    var tiny_edge_sum: u128 = 0;
+    tiny_edge_sum += try step(grid, width, grid.len - width, 64, true); // north east
+    tiny_edge_sum += try step(grid, width, grid.len - 1, 64, true); // north west
+    tiny_edge_sum += try step(grid, width, width - 1, 64, true); // south west
+    tiny_edge_sum += try step(grid, width, 0, 64, true); // south east
+    tiny_edge_sum *= 202300;
+
+    var edge_sum: u128 = 0;
+    edge_sum += try step(grid, width, grid.len - width, 65 * 3, true); // north east
+    edge_sum += try step(grid, width, grid.len - 1, 65 * 3, true); // north west
+    edge_sum += try step(grid, width, width - 1, 65 * 3, true); // south west
+    edge_sum += try step(grid, width, 0, 65 * 3, true); // south east
+    edge_sum *= 202299;
+
+    var corner_sum: u128 = 0;
+    corner_sum += try step(grid, width, grid.len - width / 2 - 1, 130, true); // north
+    corner_sum += try step(grid, width, height / 2 * width + width - 1, 130, true); // west
+    corner_sum += try step(grid, width, width / 2, 130, true); // south
+    corner_sum += try step(grid, width, height / 2 * width, 130, true); // east
+
+    const result = grid_sum + tiny_edge_sum + edge_sum + corner_sum;
+    std.debug.print("Day 21 >> {d}\n", .{result});
+}
+
+pub fn step(grid: []const u8, width: usize, start: usize, steps: usize, print: bool) !u128 {
+    var plots = std.AutoHashMap(usize, void).init(std.heap.page_allocator);
+    try plots.put(start, void{});
+    for (0..steps) |_| {
+        var new_plots = std.AutoHashMap(usize, void).init(std.heap.page_allocator);
+        defer new_plots.deinit();
+        var plots_iter = plots.keyIterator();
+        while (plots_iter.next()) |plot| {
+            if (plot.* % width != 0 and grid[plot.* - 1] != '#') try new_plots.put(plot.* - 1, void{});
+            if (plot.* % width != width - 1 and grid[plot.* + 1] != '#') try new_plots.put(plot.* + 1, void{});
+            if (plot.* + width < grid.len and grid[plot.* + width] != '#') try new_plots.put(plot.* + width, void{});
+            if (plot.* >= width and grid[plot.* - width] != '#') try new_plots.put(plot.* - width, void{});
+        }
+        plots.clearAndFree();
+        plots = try new_plots.clone();
+    }
+    if (print) {
+        print_map(grid, width, &plots);
+        std.debug.print("\n\n", .{});
+    }
+    return @intCast(plots.count());
+}
+
+pub fn print_map(grid: []const u8, width: usize, plots: *const std.AutoHashMap(usize, void)) void {
+    for (0..grid.len / width) |y| {
+        for (0..width) |x| {
+            if (x == width / 2 or y == grid.len / width / 2) {
+                std.debug.print("\x1b[31m", .{});
+            }
+            if (plots.get(x + y * width) != null) {
+                std.debug.print("O", .{});
+            } else {
+                std.debug.print("{c}", .{grid[x + y * width]});
+            }
+            std.debug.print("\x1b[0m", .{});
+        }
+        std.debug.print("\n", .{});
+    }
 }
